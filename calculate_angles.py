28fc901163c4b3a58e5d77ad837f79bd590df89d
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-12-10 20:12:17 +0000

    hopefully this fixes the linter

--- calculate_angles.py
+++ calculate_angles.py
@@ -14,15 +14,12 @@ def play_again():
     while True:
         # getting user input
         yes_no = input("Do you want to play again? (y/n)")
-
         # they want to play again
         if yes_no.lower() == "y":
             return True
-
         # they don't want to play again
         if yes_no.lower() == "n":
             return False
-
         # user input invalid
         print("please make sure your input is valid.")
 
@@ -38,19 +35,16 @@ def error_checking(user_input):
     # try catch
     try:
         user_input_fl = float(user_input)
-
     # input is invalid
     except Exception:
         print("Input invalid! Please enter a valid number!")
         return 0
-
     # user input is valid
     else:
         # user input is not positive
         if user_input_fl < 1:
             print("Side length must be at least 1 unit long!")
             return 0
-
         # user input is positive
         else:
             return user_input_fl
@@ -63,41 +57,34 @@ def main():
         leg_1_str = input("Enter the length of one of the legs (no units): ")
         if error_checking(leg_1_str) != 0:
             leg_1_fl = error_checking(leg_1_str)
-
         # user input is valid
         else:
             if play_again():
                 continue
             break
-
         # user input for the other leg
         leg_2_str = input("Enter the length of the other leg (no units): ")
         if error_checking(leg_2_str) != 0:
             leg_2_fl = error_checking(leg_2_str)
-
         # user input is invalid
         else:
             if play_again():
                 continue
             break
-
         # user input for the hypotenuse
         hypotenuse_str = input("Enter the length of the hypotenuse (no units): ")
         if error_checking(hypotenuse_str) != 0:
             hypotenuse_fl = error_checking(hypotenuse_str)
-
             # hypotenuse is not the longest side
             if hypotenuse_fl < leg_1_fl or hypotenuse_fl < leg_2_fl:
                 print("The hypotenuse needs to be the longest side!")
                 if play_again():
                     continue
                 break
-
             # all input is valid
             else:
                 theta_1 = calc_angles(leg_1_fl, hypotenuse_fl)
                 theta_2 = calc_angles(leg_2_fl, hypotenuse_fl)
-
                 print()
                 print(
                     f"The angle opposite to the side with {leg_1_str}units is {theta_1:.2f}° and the angle opposite to the side with {leg_2_str}units is {theta_2:.2f}°."
@@ -105,7 +92,6 @@ def main():
                 if play_again():
                     continue
                 break
-
         # user input is invalid
         else:
             if play_again():
